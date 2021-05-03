f0e01bc91268e0d82269a466a1057193b0245ea6
Gregory <10611760+gregoryguillou@users.noreply.github.com>
2021-05-03 23:39:32 +0200

    Brew formula update for crzy version v0.2.4

--- Formula/crzy.rb
+++ Formula/crzy.rb
@@ -5,25 +5,25 @@
 class Crzy < Formula
   desc "deploy your tests in less than 3 seconds."
   homepage "https://github.com/go-crzy/crzy"
-  version "0.2.3"
+  version "0.2.4"
   license "Apache-2.0"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/go-crzy/crzy/releases/download/v0.2.3/crzy_0.2.3_darwin_x86_64.tar.gz"
-    sha256 "6eb5cd8d8617e4dbf4c770507b2f9760daa98fbcb9559a1f61345ac0d69c1854"
+    url "https://github.com/go-crzy/crzy/releases/download/v0.2.4/crzy_0.2.4_darwin_x86_64.tar.gz"
+    sha256 "9d484193bf078f25112e897b6c19abae555cb1da6daaafeb92669c837c089eed"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/go-crzy/crzy/releases/download/v0.2.3/crzy_0.2.3_darwin_arm64.tar.gz"
-    sha256 "70805e9753593db7bec72193350174cbe486cb4ba5c6f9c603f6b0e380fe45ed"
+    url "https://github.com/go-crzy/crzy/releases/download/v0.2.4/crzy_0.2.4_darwin_arm64.tar.gz"
+    sha256 "cc7cab9b22273844afa8729534c5a02eab57f2623f15a49d0b7459cf0a253731"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/go-crzy/crzy/releases/download/v0.2.3/crzy_0.2.3_linux_x86_64.tar.gz"
-    sha256 "b8a59c6729544133552d355e0ec10b76e6f7357d83d8406a7f9d95ceb4b4235e"
+    url "https://github.com/go-crzy/crzy/releases/download/v0.2.4/crzy_0.2.4_linux_x86_64.tar.gz"
+    sha256 "165bc9ee9c20033cdd53c8f3140c9ab929087ce11e8121ac8ce84d7b3b59bdb6"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/go-crzy/crzy/releases/download/v0.2.3/crzy_0.2.3_linux_arm64.tar.gz"
-    sha256 "8473226f55f1701d79887106a6cfee5f8abbb07ba39237ee6c9a443e1dde4d3f"
+    url "https://github.com/go-crzy/crzy/releases/download/v0.2.4/crzy_0.2.4_linux_arm64.tar.gz"
+    sha256 "4ee654cc0f645e01cc478e70428b32b10c0baf412bea09e53eed49b8a51b7e09"
   end
 
   depends_on "git"
